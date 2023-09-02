f24ce29d0acc58f9b1da1be894f2aaf4534cfb35
Kauan Pereira <114433504+kauantpereira@users.noreply.github.com>
2023-09-02 10:58:59 -0300

    adição de comentários a livros.c

--- livros/livros.c
+++ livros/livros.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
 int main(){
-
+    int q, d, p; //declaração das variáveis
     int pag;
-    int q, d, p;
     
-    comeco:
+    comeco: //início do loop
     
     do{
         scanf("%i %i %i", &q, &d, &p);
-    } while (q < 0 || q > 20 || d < 0 || d > 20 || p < 0 || p > 20);
+    } while (q < 0 || q > 20 || d < 0 || d > 20 || p < 0 || p > 20); //só aceita valores dentro do espaço estabelecido
 
-    if (q != 0 && d != 0 && p != 0){
+    if (q != 0 && d != 0 && p != 0){ //se nenhum dos valores é zero, executa normalmente
         
-        pag = (d * q * p)/(p - q);
+        pag = (d * q * p)/(p - q); //fórmula utiizada
         
-        if(pag == 1){
+        if(pag == 1){ //imprime a resposta no singular
             printf("%i pagina\n", pag);
-        } else {
+        } else { //imprime a resposta no plural
             printf("%i paginas\n", pag);
         }
-        goto comeco;
         
-        } else {
+        goto comeco; //recebe novos valores e repete o processo
+        
+        } else { //se pelo menos um dos valores é zero, encerra o programa
             return 0;
         }            
 }
